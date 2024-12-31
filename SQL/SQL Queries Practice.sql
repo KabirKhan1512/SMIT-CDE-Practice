82f6440962b27ba8383f5b2f816e9855ca4f3666
KabirKhan1512 <kabirkhan1512@gmail.com>
2024-12-31 20:13:10 +0500

    Completing Assignments

--- SQL/SQL Queries Practice.sql
+++ SQL/SQL Queries Practice.sql
@@ -76,3 +76,71 @@ select category_id, avg(list_price)
 from production.products
 group by
 category_id;
+
+select category_id, avg(list_price)
+from production.products
+group by
+category_id;
+
+-- Assignment 8
+select left(product_name,3) as '3 char'
+from production.products;
+
+select CHARINDEX('Townie',product_name) as 'char'
+from production.products;
+
+select product_name
+from production.products;
+
+-- Assignment 9
+select RIGHT(phone,4)
+from sales.customers
+where phone is not null;
+
+select REPLACE(product_name,'Townie','Town') as replaced
+from production.products;
+
+-- Assignment 10
+select *
+from sales.orders
+where order_date between '2017-01-01' and '2017-12-31';
+
+select *
+from sales.customers
+where state not in ('NY', 'TX');
+
+--Assignment 11
+select first_name, last_name, order_status, store_id
+from sales.customers
+inner join sales.orders
+on sales.customers.customer_id = sales.orders.customer_id;
+
+--Assignment 12
+select * from production.products;
+select * from production.categories;
+
+select product_name, model_year, category_name
+from production.products
+left join production.categories
+on production.products.category_id = production.categories.category_id;
+
+
+select product_name, avg(list_price) as average
+from production.products
+group by product_name
+having avg(list_price) <= 200;
+
+select customer_id, count(customer_id)
+from sales.orders
+group by customer_id
+having COUNT(customer_id) > 2;
+
+-- Assignment 13
+select *
+from production.products
+order by
+len(product_name);
+
+
+
+
