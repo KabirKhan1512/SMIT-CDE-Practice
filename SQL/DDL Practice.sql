f2661053c122ffec74362bc194d61dfb5ddd9fcc
KabirKhan1512 <kabirkhan1512@gmail.com>
2025-03-03 02:51:43 +0500

    sql

--- SQL/DDL Practice.sql
+++ SQL/DDL Practice.sql
@@ -1,8 +1,4 @@
-create database companydb;
 
-use companydb;
-
-create schema HR;
 
 create table hr.employees (
 	employeeID int primary key identity(1,1) not null,
@@ -23,9 +19,6 @@ create table hr.departments (
 	departmentID int primary key identity(1,1) not null,
 	departmentname varchar(20) unique not null,
 	managerID int,
-	foreign key (managerID)
-		references hr.employees (employeeID)
-		on delete cascade on update cascade
 )
 
 create table hr.projects (
@@ -36,14 +29,55 @@ create table hr.projects (
 	budget int check(budget > 10000)
 )
 
-create table employeesprojects (
+create table hr.employeesprojects (
 	employeeID int,
 	projectID int,
 	assigneddate date not null,
 	foreign key (employeeID)
 		references hr.employees (employeeID)
-		on delete cascade on update cascade
+		on delete cascade on update cascade,
 	foreign key (projectID)
 		references hr.projects (projectID)
 		on delete cascade on update cascade
 )
+
+
+
+ALTER TABLE hr.departments 
+ADD CONSTRAINT fk_manager 
+FOREIGN KEY (managerID)
+REFERENCES hr.employees (employeeID);
+
+
+select * from hr.departments;
+
+insert into hr.departments (departmentname, managerID)
+values ('IT', null),
+		('Ac', null),
+		('Fin', null),
+		('Mark', null);
+
+
+
+insert into hr.employees (first_name,second_name,email,phone,hiredate,jobtitle,salary,departmentID) 
+	values ('kabir', 'khan', 'abc', '0312', GETDATE(), 'accountant', 30000, 2);
+
+insert into hr.employees (first_name,second_name,email,phone,hiredate,jobtitle,salary,departmentID) 
+	values ('ali', 'khan', 'asdc', '0334', GETDATE(), 'Financer', 60000, 3);
+
+
+
+select * from hr.employeesprojects;
+select * from hr.employees;
+select * from hr.projects;
+
+insert into hr.projects (projectname, startdate,enddate,budget)
+values 
+ ('masjid', getdate(),DATEADD(MONTH, 9, GETDATE()), 600000)
+;
+
+s
+
+
+
+
